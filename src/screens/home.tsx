07c34257bbf390c0fc6b67c66e5b18bbce78a170
Gabriel Carniel <gpcarniel@outlook.com>
2024-09-14 15:33:52 -0300

    finalizando home

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,9 +1,60 @@
-import { Center, Text } from '@gluestack-ui/themed'
+import { ExerciseCard } from '@components/exercise-card'
+import { Group } from '@components/group'
+import { HomeHeader } from '@components/home-header'
+import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
 
 export function Home() {
+  const [exercises] = useState([
+    'Puxada frontal',
+    'Remada curvada',
+    'Remada unilateral',
+    'Levantamento terra',
+    'Levantamento terra 2',
+    'Levantamento terra 3',
+    'Levantamento terra 4',
+  ])
+  const [groups] = useState(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [groupSelected, setGroupSelected] = useState('Costas')
   return (
-    <Center flex={1}>
-      <Text>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toLowerCase() === item.toLowerCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingHorizontal: 10, gap: 10 }}
+        style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
+      />
+
+      <VStack px="$8" flex={1}>
+        <HStack justifyContent="space-between" mb="$5" alignItems="center">
+          <Heading color="$gray200" fontSize="$md">
+            Exercícios
+          </Heading>
+          <Text color="$gray200" fontSize="$sm" fontFamily="$body">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={() => <ExerciseCard />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
+    </VStack>
   )
 }

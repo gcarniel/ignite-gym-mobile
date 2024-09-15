1904ff01f2645def06945f878085c16f19f53b95
Gabriel Carniel <gpcarniel@outlook.com>
2024-09-15 14:28:01 -0300

    alterando imagem do perfil

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -2,10 +2,77 @@ import { Button } from '@components/button'
 import { Input } from '@components/input'
 import { ScreenHeader } from '@components/screen-header'
 import { UserPhoto } from '@components/user-photo'
-import { Center, Heading, ScrollView, Text, VStack } from '@gluestack-ui/themed'
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Text,
+  useToast,
+  VStack,
+} from '@gluestack-ui/themed'
 import { TouchableOpacity } from 'react-native'
 
+import { useState } from 'react'
+
+import * as FileSystem from 'expo-file-system'
+import * as ImagePicker from 'expo-image-picker'
+import { ToastMessage } from '@components/toast-message'
+
 export function Profile() {
+  const [userPhoto, setUserPhoto] = useState('https://github.com/gcarniel.png')
+  const toast = useToast()
+  const handlePhotoSelect = async () => {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      })
+
+      if (photoSelected.canceled) return
+
+      const photoURI = photoSelected.assets[0].uri
+
+      if (photoURI) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoURI)) as {
+          size: number
+        }
+
+        if (photoInfo.size / 1024 / 1024 > 5) {
+          toast.show({
+            placement: 'top',
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Limite de imagem excedido"
+                description="Imagem muito grande, escolha uma de até 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          })
+          return
+        }
+
+        setUserPhoto(photoURI)
+      }
+    } catch (error) {
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title="Algo deu errado, tente novamente"
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+      console.warn(error)
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,12 +80,12 @@ export function Profile() {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: 'https://github.com/gcarniel.png' }}
+            source={{ uri: userPhoto }}
             size="xl"
             alt="Imagem do usuário"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handlePhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"

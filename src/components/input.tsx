71a3e900317b4b0b8587e67dfa3e1c658cc5be32
Gabriel Carniel <gpcarniel@outlook.com>
2024-09-15 13:19:16 -0300

    alteracao no input

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,21 +1,25 @@
 import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
 import { ComponentProps } from 'react'
 
-interface InputProps extends ComponentProps<typeof InputField> {}
+interface InputProps extends ComponentProps<typeof InputField> {
+  isReadOnly?: boolean
+}
 
-export function Input({ ...props }: InputProps) {
+export function Input({ isReadOnly = false, ...props }: InputProps) {
   return (
     <GluestackInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderColor="$gray600"
       borderRadius="$md"
       $focus={{
         borderColor: '$green600',
       }}
+      isReadOnly={isReadOnly}
+      opacity={isReadOnly ? 0.5 : 1}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"

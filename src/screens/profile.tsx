76a3435872e4056650e9a5ec33edac3fcf6787a6
Gabriel Carniel <gpcarniel@outlook.com>
2024-09-15 13:19:27 -0300

    pagina de profile finalizada

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,9 +1,64 @@
-import { Center, Text } from '@gluestack-ui/themed'
+import { Button } from '@components/button'
+import { Input } from '@components/input'
+import { ScreenHeader } from '@components/screen-header'
+import { UserPhoto } from '@components/user-photo'
+import { Center, Heading, ScrollView, Text, VStack } from '@gluestack-ui/themed'
+import { TouchableOpacity } from 'react-native'
 
 export function Profile() {
   return (
-    <Center flex={1}>
-      <Text>Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+        <Center mt="$6" px="$10">
+          <UserPhoto
+            source={{ uri: 'https://github.com/gcarniel.png' }}
+            size="xl"
+            alt="Imagem do usuário"
+          />
+
+          <TouchableOpacity>
+            <Text
+              color="$green500"
+              fontFamily="$heading"
+              fontSize="$md"
+              mt="$2"
+              mb="$8"
+            >
+              Alterar Foto
+            </Text>
+          </TouchableOpacity>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Nome" bg="$gray600" />
+            <Input value="gcarniel@outlook.com" bg="$gray600" isReadOnly />
+          </Center>
+
+          <Heading
+            alignSelf="flex-start"
+            fontFamily="$heading"
+            color="$gray200"
+            fontSize="$md"
+            mt="$12"
+            mb="$2"
+          >
+            Alterar senha
+          </Heading>
+
+          <Center w="$full" gap="$4">
+            <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+            <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+            <Input
+              placeholder="Confirme a nova senha"
+              bg="$gray600"
+              secureTextEntry
+            />
+
+            <Button title="Atualizar" />
+          </Center>
+        </Center>
+      </ScrollView>
+    </VStack>
   )
 }
